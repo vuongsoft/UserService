1b3dc411587fdd2e4c21f1dcb038676803bfd013
Stephen <vuongsoft.vn@gmail.com>
2021-09-24 15:45:31 +0700

    New Postgres database

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -19,10 +19,10 @@ public func configure(_ app: Application) throws {
   
   app.databases.use(.postgres(
     hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-    port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-    username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-    password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-    database: Environment.get("DATABASE_NAME") ?? "vapor_database"
+    port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? 54320,
+    username: Environment.get("DATABASE_USERNAME") ?? "admin",
+    password: Environment.get("DATABASE_PASSWORD") ?? "admin",
+    database: Environment.get("DATABASE_NAME") ?? "User_database"
   ), as: .psql)
 
   app.migrations.add(CreateUser())
